fff88928386c00e60f78d07b386ca42bd506cd3c
Muncherkin <48158637+Muncherkin@users.noreply.github.com>
2023-11-02 14:03:27 +0100

    lion cage fixed 3

--- src/tt_um_muncherkin_lioncage.v
+++ src/tt_um_muncherkin_lioncage.v
@@ -13,19 +13,17 @@ module tt_um_muncherkin_lioncage (
 
     wire reset = ! rst_n;
     wire [6:0] led_out;
+    wire G_one = uio_in[2]; 
+    wire G_two = uio_in[3];
     assign uo_out[6:0] = led_out;
     assign uo_out[7] = 0;
-    wire G_one; 
-    assign uio_in[2] = G_one;
-    wire G_two;
-    assign uio_in[3] = G_two; 
-    reg state = 0;
 
     // use bidirectionals as inputs
     assign uio_oe = 0;
 
     //Max 15 lions
     reg [3:0] lion_counter;
+    reg state;
 
     always @(posedge clk) begin
         if (reset) begin
@@ -37,7 +35,11 @@ module tt_um_muncherkin_lioncage (
                 if (G_one) begin 
                    state <= 1;
                    if (! G_two) begin 
-                    lion_counter <= lion_counter + 1; 
+                    if(lion_counter == 15) begin
+                        lion_counter <=0;
+                    end else begin
+                        lion_counter <= lion_counter + 1; 
+                    end
                    end
                 end
             end
@@ -45,7 +47,11 @@ module tt_um_muncherkin_lioncage (
                 if (! G_one) begin 
                    state <= 0;
                    if (! G_two) begin 
-                     lion_counter <= lion_counter - 1; 
+                    if(lion_counter == 0) begin
+                        lion_counter <=15;
+                    end else begin
+                        lion_counter <= lion_counter - 1; 
+                    end
                    end
                 end
             end
